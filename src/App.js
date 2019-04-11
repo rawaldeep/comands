119ce6c997f5884fa2bb9336fbc031cf0ebb779c
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-04-11 14:15:27 +0200

    functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Ingredients from './components/Ingredients';
-// import Order from './components/Order';
 
 class App extends Component {
   constructor(props){
